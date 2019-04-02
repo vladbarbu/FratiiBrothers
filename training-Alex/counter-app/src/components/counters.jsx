befdac9526daf05e1109585712860b944d7a3948
alexmocanu07 <46425351+alexmocanu07@users.noreply.github.com>
2019-04-02 22:13:28 +0300

    Tutorial+AddButton

---
+++ training-Alex/counter-app/src/components/counters.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  render() {
+    const { onReset, counters, onDelete, onIncrement, onAdd } = this.props;
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        <button onClick={onAdd} className="btn btn-primary btn-sm m-2">
+          Add
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
