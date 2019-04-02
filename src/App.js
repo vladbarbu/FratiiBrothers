db76e5a48d596c539a920da2761979b4d35b67e2
Vlad Barbu <vladbarbu14@yahoo.com>
2019-04-02 14:37:25 +0300

    react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import NavBar from "./Components/navbar";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <NavBar />;
+  }
+}
+
+export default App;
