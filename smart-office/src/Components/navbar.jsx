2c2d9fbe0289e1e73f4197087b7c0dcb754db4c7
Vlad Barbu <vladbarbu14@yahoo.com>
2019-04-02 22:56:47 +0300

    Navbar

--- smart-office/src/Components/navbar.jsx
+++ smart-office/src/Components/navbar.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NavBar = ({ totalCounters }) => {
-  return (
-    <nav className="navbar navbar-light bg-light">
-      <a className="navar-brand" href="http://localhost:3000">
-        Navbar
-        <span className="badge badge-pill badge-secondary m-2">
-          {totalCounters}
-        </span>
-      </a>
-    </nav>
-  );
-};
+class NavBar extends Component {
+  render() {
+    const styleTitle = {
+      color: "#0DD2A3"
+    };
+    const styleButton = { color: "#0DD2A3" };
+    const styleSearch = { width: "500px", height: "50px" };
+    return (
+      <nav className="navbar navbar-light bg-light justify-content-between">
+        <a className="navbar-brand font-weight-bold" style={styleTitle}>
+          SmartOffice
+        </a>
+
+        <form className="form-inline">
+          <button
+            className="btn  my-2 my-sm-0 m-2 "
+            type="submit"
+            style={styleButton}
+          >
+            <i className="far fa-bell" />
+          </button>
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search for a specific item"
+            aria-label="Search"
+            width="20px"
+            style={styleSearch}
+          />
+        </form>
+      </nav>
+    );
+  }
+}
 
 export default NavBar;
