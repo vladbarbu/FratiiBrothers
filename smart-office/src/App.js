5e266f1602d0737677295ed87c487af255a66d87
Bloogdan <bogdanbadarau72@gmail.com>
2019-04-24 14:43:13 +0300

    reparat niste functii

--- smart-office/src/App.js
+++ smart-office/src/App.js
@@ -5,19 +5,36 @@ import SideBar from "./Components/sideBar";
 import NavBar from "./Components/navbar";
 
 import Element from "./model/Element";
+import Notification from "./model/Notification";
+// import Item from "./Components/Item";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     let elements = this.loadElements();
-
+    let notifications = this.loadNotifications(elements);
     this.state = {
       elements: elements,
+      notifications: notifications,
       chosen: null,
       navBarClick: false
     };
   }
+
+  loadNotifications = elements => {
+    let data = [];
+    for (let i = 0; i < elements.length; i++)
+      for (let j = 0; j < elements[i].elements.length; j++)
+        for (let k = 0; k < elements[i].elements[j].elements.length; k++) {
+          let notifications =
+            elements[i].elements[j].elements[k]["notifications"];
+          for (let n = 0; n < notifications.length; n++)
+            // data.push(new Notification(notifications[n]));
+            data.push(notifications[n]);
+        }
+    return data;
+  };
 
   loadElements = () => {
     /**
@@ -103,15 +120,17 @@ class App extends Component {
   };
 
   onItemClickMaiSmechera = (ID, V) => {
+    let found = null;
     if (V != null) {
       for (let i = 0; i < V.length; i++) {
         if (V[i].ID === ID) {
           return V[i];
         } else if (V[i].type !== "item")
-          return this.onItemClickMaiSmechera(ID, V[i].elements);
+          found = this.onItemClickMaiSmechera(ID, V[i].elements);
+          if(found !== null) break;
       }
     }
-    return null;
+    return found;
   };
 
   onItemClick = ID => {
