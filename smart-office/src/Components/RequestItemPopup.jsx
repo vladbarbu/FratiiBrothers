b6f74956b4ac7266578a7229c0d8e2a8e6929f38
Vlad Barbu <vladbarbu14@yahoo.com>
2019-04-24 21:24:06 +0300

    Update RequestItemPopup.jsx

--- smart-office/src/Components/RequestItemPopup.jsx
+++ smart-office/src/Components/RequestItemPopup.jsx
@@ -3,10 +3,39 @@ import RequestForm from "./RequestForm";
 
 import "../resources/styles/RequestPopup.css";
 class RequestItemPopup extends Component {
+  constructor(props) {
+    super(props);
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+    console.log(node);
+  }
+  /**
+   * Alert if clicked on outside of element
+   */
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      console.log("You clicked outside of me!");
+      //this.state.inputValue = "";
+      this.props.togglePopup();
+    }
+  }
+
   render() {
     return (
       <div className="requestPopup">
-        <div className="requestPopup_inner">
+        <div className="requestPopup_inner" ref={this.setWrapperRef}>
           <h2>Request new item</h2>
           <button onClick={() => this.props.togglePopup()}>
             Return to dashboard
