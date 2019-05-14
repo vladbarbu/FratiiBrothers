00d1d0ffa632f4b852ef81d35dd4646f336852a1
Vlad Barbu <vladbarbu14@yahoo.com>
2019-05-14 20:48:24 +0300

    Calendar demo

---
+++ smart-office-manager/src/Components/Calendar.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import dateFns from "date-fns";
+import "../resources/styles/Calendar.scss";
+
+class Calendar extends React.Component {
+  state = {
+    currentMonth: new Date(),
+    selectedDate: new Date(),
+    currentDay: new Date()
+  };
+
+  renderHeader() {
+    const dateFormat = "MMMM YYYY";
+
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-center">
+          <span>{dateFns.format(this.state.currentMonth, dateFormat)}</span>
+        </div>
+      </div>
+    );
+  }
+
+  renderDays() {
+    const dateFormat = "dddd";
+    const days = [];
+
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+
+    return <div className="days row">{days}</div>;
+  }
+
+  renderCells() {
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+
+    const dateFormat = "D";
+    const rows = [];
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = dateFns.format(day, dateFormat);
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`col cell ${
+              !dateFns.isSameMonth(day, monthStart)
+                ? "disabled"
+                : dateFns.isSameDay(day, selectedDate)
+                ? "selected"
+                : ""
+            }`}
+            key={day}
+            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+            <div className="myCalendarText">
+              <div className="realStock">
+                <i className="material-icons">fiber_manual_record</i>
+                <small className="stockAmount">100</small>
+              </div>
+              <div className="predictedStock">
+                <i className="material-icons">fiber_manual_record</i>
+                <small className="stockAmount">100</small>
+              </div>
+            </div>
+          </div>
+        );
+        day = dateFns.addDays(day, 1);
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  }
+
+  onDateClick = day => {
+    var day = this.state.currentDay;
+    this.setState({
+      selectedDate: day
+    });
+  };
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    });
+  };
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    });
+  };
+
+  render() {
+    return (
+      <div className="calendar">
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
+  }
+}
+
+export default Calendar;
--- smart-office-manager/src/Components/SupplyStatistics.jsx
+++ smart-office-manager/src/Components/SupplyStatistics.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../resources/styles/SupplyStatistics.scss";
+import Calendar from "./Calendar";
 class SupplyStatistics extends Component {
   state = {
     pressed: "Calendar"
@@ -30,45 +31,46 @@ class SupplyStatistics extends Component {
           <i class="material-icons">show_chart</i>
           Supply Statistics
         </div>
-        <div>
-          <div className="supplyButtons">
-            {this.state.pressed === "Calendar" ? (
-              <small
-                className="supplyButton"
-                onClick={() => this.setPressed("Calendar")}
-                style={{ backgroundColor: "#F9E816" }}
-              >
-                <i class="material-icons">calendar_today</i>Supply Calendar
-              </small>
-            ) : (
-              <small
-                className="supplyButton"
-                onClick={() => this.setPressed("Calendar")}
-              >
-                <i class="material-icons">calendar_today</i>Supply Calendar
-              </small>
-            )}
+        <div className="supplyButtons">
+          {this.state.pressed === "Calendar" ? (
+            <small
+              className="supplyButton"
+              onClick={() => this.setPressed("Calendar")}
+              style={{ backgroundColor: "#F9E816" }}
+            >
+              <i class="material-icons">calendar_today</i>Supply Calendar
+            </small>
+          ) : (
+            <small
+              className="supplyButton"
+              onClick={() => this.setPressed("Calendar")}
+            >
+              <i class="material-icons">calendar_today</i>Supply Calendar
+            </small>
+          )}
 
-            {this.state.pressed === "Graph" ? (
-              <small
-                className="supplyButton"
-                onClick={() => this.setPressed("Graph")}
-                style={{ backgroundColor: "#F9E816" }}
-              >
-                {" "}
-                <i class="material-icons">graphic_eq</i>Supply Graph
-              </small>
-            ) : (
-              <small
-                className="supplyButton"
-                onClick={() => this.setPressed("Graph")}
-              >
-                {" "}
-                <i class="material-icons">graphic_eq</i>Supply Graph
-              </small>
-            )}
-          </div>
+          {this.state.pressed === "Graph" ? (
+            <small
+              className="supplyButton"
+              onClick={() => this.setPressed("Graph")}
+              style={{ backgroundColor: "#F9E816" }}
+            >
+              {" "}
+              <i class="material-icons">graphic_eq</i>Supply Graph
+            </small>
+          ) : (
+            <small
+              className="supplyButton"
+              onClick={() => this.setPressed("Graph")}
+            >
+              {" "}
+              <i class="material-icons">graphic_eq</i>Supply Graph
+            </small>
+          )}
         </div>
+        {this.state.pressed === "Calendar" ? (
+          <Calendar className="supplyCalendar" />
+        ) : null}
       </div>
     );
   }
